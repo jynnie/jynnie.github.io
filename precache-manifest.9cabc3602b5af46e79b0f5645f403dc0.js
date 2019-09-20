8b46bea4a22e53c04b9dd0240351574a7e478eb7
jynnie <jynnie@mit.edu>
2019-09-20 10:35:36 -0400

    temp new react app

---
+++ precache-manifest.9cabc3602b5af46e79b0f5645f403dc0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd28c856be915cb1d11f07d343782e37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "464b03c0bc2ff2bec24a",
+    "url": "/static/css/main.aa5c4ed1.chunk.css"
+  },
+  {
+    "revision": "2a4b16b1ddbdbd0b9a35",
+    "url": "/static/js/2.a6de7931.chunk.js"
+  },
+  {
+    "revision": "464b03c0bc2ff2bec24a",
+    "url": "/static/js/main.b431a9d9.chunk.js"
+  },
+  {
+    "revision": "b70565ae6bbbe783a6e0",
+    "url": "/static/js/runtime~main.dce780dc.js"
+  },
+  {
+    "revision": "f1775318037b818994caa874c984f7b7",
+    "url": "/static/media/2E.f1775318.png"
+  },
+  {
+    "revision": "382693528911ef3e1a07124a930f6b68",
+    "url": "/static/media/BeforeBlood.38269352.png"
+  },
+  {
+    "revision": "5f929478ecbb4e7a382d66e9bff60e96",
+    "url": "/static/media/DoS.5f929478.png"
+  },
+  {
+    "revision": "7c8914db81510e19adc23f4a23dfa304",
+    "url": "/static/media/HackLogo.7c8914db.png"
+  },
+  {
+    "revision": "56c1f3151d88b0aae42dc4a68ee2251a",
+    "url": "/static/media/PlixBit.56c1f315.png"
+  },
+  {
+    "revision": "3e861017617e650cb7143aa43ecfca93",
+    "url": "/static/media/Sigchain.3e861017.png"
+  },
+  {
+    "revision": "849ef871c1e7180a6bcf21929dedaf4e",
+    "url": "/static/media/alight-clip.849ef871.gif"
+  },
+  {
+    "revision": "23ad46b0cb7902a34a14927066965bb6",
+    "url": "/static/media/cityneon.23ad46b0.gif"
+  },
+  {
+    "revision": "cac59dccdcbd4fcc59d07f65026c0134",
+    "url": "/static/media/labyrinth-box.cac59dcc.jpg"
+  },
+  {
+    "revision": "bf66c5eec4f433cb31edbf4aad56816c",
+    "url": "/static/media/onionvale.bf66c5ee.png"
+  },
+  {
+    "revision": "87f15b3ed5aabf64a8a4cf0687b93438",
+    "url": "/static/media/spectre.87f15b3e.gif"
+  },
+  {
+    "revision": "3dfc907b9248e28023b0937cc455f393",
+    "url": "/static/media/summoner.3dfc907b.png"
+  }
+]);
